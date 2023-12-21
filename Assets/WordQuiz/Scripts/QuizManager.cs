fd1769e174a11b96613ec79ab192e7e51c0f2800
Bilal-Yucell <bilallyucell@gmail.com>
2023-12-21 05:51:39 +0300

    Added QuizManager and WordData Scripts

---
+++ Assets/WordQuiz/Scripts/QuizManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+    public static QuizManager instance;
+
+    // [SerializeField] private GameObject gameComplete;
+    //Scriptable data which store our questions data
+    // [SerializeField] private QuizDataScriptable questionDataScriptable;
+    // [SerializeField] private Image questionImage;           //image element to show the image
+
+    [SerializeField] private WordData[] answerWordList;     //list of answers word in the game
+    [SerializeField] private WordData[] optionsWordList;    //list of options word in the game
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    [System.Serializable]
+    public class QuestionData
+    {
+        public Sprite questionImage;
+        public string answer;
+    }
+
+}
